3a31372f77eb8f16cf85ecda6031f014946a3e68
hotlove_linx <guolejian@126.com>
2019-06-20 21:59:48 +0800

    websocket

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.12
 
 require (
 	github.com/Go-SQL-Driver/MySQL v1.4.1
-	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/gorilla/websocket v1.4.0
 	gopkg.in/yaml.v2 v2.2.2
 )
--- main.go
+++ main.go
@@ -4,30 +4,11 @@ import (
 	"fmt"
 	_ "go_database/entity"
 	"go_database/server/socket"
-	"log"
+	"go_database/server/websocket"
 	"net"
-	"net/http"
 	"os"
-	"github.com/golang/net/websocket"
 )
 
-func Echo(ws *websocket.Conn) {
-	var err error
-	for {
-		var reply string
-		if err = websocket.Message.Receive(ws, &reply); err != nil {
-			fmt.Println("Can't receive")
-			break
-		}
-		fmt.Println("Received back from client: " + reply)
-		msg := "Received: " + reply
-		fmt.Println("Sending to client: " + msg)
-		if err = websocket.Message.Send(ws, msg); err != nil {
-			fmt.Println("Can't send")
-			break
-		}
-	}
-}
 func main() {
 	// 启动默认http服务
 	//http.Start()
@@ -39,10 +20,7 @@ func main() {
 	//socket.NewSocketClient()
 
 	// websocket
-	http.Handle("/", websocket.Handler(Echo))
-	if err := http.ListenAndServe(":1234", nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
+	websocket.StartServer()
 }
 
 func startSocketServer() {
