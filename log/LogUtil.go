0fcfcfc4cdb75745dec0c13035614825d65bd6bc
guolejian <guolejian@126.com>
2019-08-14 20:23:46 +0800

    自定义日志

---
+++ log/LogUtil.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	trace  *log.Logger
+	info   *log.Logger
+	error  *log.Logger
+	waring *log.Logger
+)
+
+func init() {
+	trace = log.New(ioutil.Discard,
+		"TRACE:",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	info = log.New(os.Stdout,
+		"INFO:",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	error = log.New(io.MultiWriter(os.Stderr, os.Stdout),
+		"ERROR:",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	waring = log.New(os.Stdout,
+		"WARING:",
+		log.Ldate|log.Lmicroseconds|log.Llongfile)
+}
+
+func Info(message string) {
+	info.Println(message)
+}
+
+func Error(message string) {
+	error.Println(message)
+}
+
+func Waring(message string) {
+	waring.Println(message)
+}
+
+func Trace(message string) {
+	trace.Println(message)
+}
--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	_ "go_database/entity"
+	Logger "go_database/log"
 	"go_database/server/socket"
 	"net"
 	"os"
-	"time"
 )
 
 func main() {
@@ -27,15 +27,33 @@ func main() {
 	//fmt.Println(value)
 
 	// 定时器
-	tick := time.NewTicker(1 * time.Second)
+	//tick := time.NewTicker(1 * time.Second)
+	//
+	//for {
+	//	select {
+	//	case dateTime := <-tick.C:
+	//		// 这里面有格式化时间在里面
+	//		fmt.Println(dateTime.Format("2006/01/02 15/04/05"), "执行了")
+	//	}
+	//}
 
-	for {
-		select {
-		case dateTime := <-tick.C:
-			// 这里面有格式化时间在里面
-			fmt.Println(dateTime.Format("2006/01/02 15/04/05"), "执行了")
-		}
-	}
+	// 日志
+	// Println 写到标准日志记录器
+	//log.Println("message")
+	//
+	//// Fatalln 在调用println 之后紧接着调用os.Exit(1)
+	//log.Fatalln("fatal message")
+	//
+	////
+	//log.Panicln("panic message")
+
+	Logger.Info("info message")
+
+}
+
+func init() {
+	//log.SetPrefix("TRACE:")
+	//log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
 }
 
 func startSocketServer() {
